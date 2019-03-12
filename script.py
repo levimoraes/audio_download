634859981dffbb2194af685d4c43db73e9e9093a
Levi Moraes <levi.moraes@yahoo.com.br>
2019-03-12 14:54:04 -0300

    Best practices

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 import sys
 import subprocess
 
-
-
 def check_node_status(node_name):
 	cmd = "wget https://www.rioaliancafrancesa.com.br/captacao/wp-content/uploads/2018/06/piste-002.mp3"
 	return 	subprocess.check_output(cmd, shell=True)
